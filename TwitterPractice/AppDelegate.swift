f2acc3604c925682b55f78729129e1e37dd4a93e
Antonio081014 <antonio081014@antonio081014.com>
2017-08-22 14:24:46 -0700

    Reorganize project.

--- TwitterPractice/AppDelegate.swift
+++ TwitterPractice/AppDelegate.swift
@@ -16,6 +16,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.makeKeyAndVisible()
+        
+        let layout = UICollectionViewFlowLayout()
+        let homeViewController = HomeViewController(collectionViewLayout: layout)
+        self.window?.rootViewController = UINavigationController(rootViewController: homeViewController)
         return true
     }
 
---
+++ TwitterPractice/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  TwitterPractice
+//
+//  Created by Antonio081014 on 8/22/17.
+//  Copyright © 2017 Antonio081014.com. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UICollectionViewController {
+
+    private let cellID = "cellID"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.collectionView?.backgroundColor = .green
+  
+    }
+}
+
