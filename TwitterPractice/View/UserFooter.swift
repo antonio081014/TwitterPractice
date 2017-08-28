d8a2067535af959eae8bed43c3c2fb607c87dc87
Antonio081014 <antonio081014@antonio081014.com>
2017-08-28 11:29:35 -0700

    Add UserFooter and UserHeader

---
+++ TwitterPractice/View/UserFooter.swift
@@ -0,0 +1,16 @@
+//
+//  UserFooter.swift
+//  TwitterPractice
+//
+//  Created by Antonio081014 on 8/28/17.
+//  Copyright © 2017 Antonio081014.com. All rights reserved.
+//
+
+import UIKit
+
+class UserFooter: BaseCell {
+    override func setupViews() {
+        super.setupViews()
+        self.backgroundColor = .green
+    }
+}
---
+++ TwitterPractice/View/UserHeader.swift
@@ -0,0 +1,15 @@
+//
+//  UserHeader.swift
+//  TwitterPractice
+//
+//  Created by Antonio081014 on 8/28/17.
+//  Copyright © 2017 Antonio081014.com. All rights reserved.
+//
+
+import UIKit
+class UserHeader: BaseCell {
+    override func setupViews() {
+        super.setupViews()
+        self.backgroundColor = .blue
+    }
+}
